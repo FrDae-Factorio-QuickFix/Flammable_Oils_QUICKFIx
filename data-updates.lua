e30108a2ed6fa6dd4742a048e093c6fe79e76bd5
Fr_Dae <dae.anthony@gmail.com>
2025-03-04 19:15:38 +0100

    0.2.009

--- data-updates.lua
+++ data-updates.lua
@@ -1,19 +1,31 @@
-local fire = table.deepcopy(data.raw.fire["fire-flame"])
+require "util"
+local fire = util.table.deepcopy(data.raw.fire["fire-flame"])
 fire.initial_lifetime = 600
 fire.name="oil-fire-flame"
 fire.damage_per_tick = {amount = 1, type = "fire"},
 data:extend({fire})
 
 local fuel_values = {
-  ["crude-oil"] = "2.5MJ",
-  ["light-oil"] = "3MJ",
-  ["heavy-oil"] = "2MJ",
-  ["petroleum-gas"] = "3MJ",
-  ["diesel-fuel"] = "4MJ",
+  ["crude-oil"] = "0.4MJ",
+  ["light-oil"] = "0.9MJ",
+  ["heavy-oil"] = "0.45MJ",
+  ["petroleum-gas"] = "0.45MJ",
+  ["diesel-fuel"] = "1.1MJ"
+  }
+local emissions = {
+  ["crude-oil"] = 1.4,
+  ["light-oil"] = 1.2,
+  ["heavy-oil"] = 1.3,
+  ["petroleum-gas"] = 1,
+  ["diesel-fuel"] = 0.8
 }
 
 for k, fluid in pairs (data.raw.fluid) do
   if not fluid.fuel_value then
     fluid.fuel_value = fuel_values[fluid.name]
+end
+if not    
+fluid.emissions_multiplier then
+    fluid.emissions_multiplier = emissions[fluid.name]
   end
 end
