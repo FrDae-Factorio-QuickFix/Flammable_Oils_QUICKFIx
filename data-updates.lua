4d9d3b34c2fe82dcb9946e9b3444fef1d75cb443
IonShieldQuad <IonShieldQuad@users.noreply.github.com>
2025-03-13 23:58:51 +0300

    More BOOM

--- data-updates.lua
+++ data-updates.lua
@@ -1,9 +1,17 @@
 require "util"
 local fire = util.table.deepcopy(data.raw.fire["fire-flame"])
-fire.initial_lifetime = 600
+fire.initial_lifetime = 1200
 fire.name="oil-fire-flame"
-fire.damage_per_tick = {amount = 1, type = "fire"}
+fire.damage_per_tick = {amount = 2, type = "fire"}
+---@diagnostic disable-next-line: assign-type-mismatch
 data:extend({fire})
+
+--note: 
+--base pipes have 100 hp and 70% fire resist
+--undergrounds have 150 hp and 80% fire resist
+--as such we need 150 / (1 - 0.8) = 750 fire amage do guarantee destruction
+
+
 
 local fuel_values = {
   ["crude-oil"] = "0.4MJ",
@@ -27,11 +35,11 @@ local emissions = {
 }
 
 for k, fluid in pairs (data.raw.fluid) do
-  if not fluid.fuel_value then
+  if not fluid.fuel_value or fluid.fuel_value == "0J" then --All fluids have 0J fuel value by default
     fluid.fuel_value = fuel_values[fluid.name]
 end
 if not    
-fluid.emissions_multiplier then
+      fluid.emissions_multiplier and fluid.emissions_multiplier ~= 1.0 then --All fluids have 1.0 multiplier by default
     fluid.emissions_multiplier = emissions[fluid.name]
   end
 end
